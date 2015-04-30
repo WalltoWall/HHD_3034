76f51355adb2fb2a1ba5d6bf2a97d21f0613d272
Mark Dean Guillermo <mguiller@gmail.com>
2015-04-29 17:10:44 -1000

    disable animsition

--- js/app.js
+++ js/app.js
@@ -5,29 +5,29 @@
 $(document).foundation();
 $(document).ready(function() {
 
-  $(".animsition").animsition({
+  // $(".animsition").animsition({
     
-      inClass               :   'fade-in',
-      outClass              :   'fade-out',
-      inDuration            :    600,
-      outDuration           :    400,
-      linkElement           :   '.animsition-link',
-      // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
-      loading               :    true,
-      loadingParentElement  :   'body', //animsition wrapper element
-      loadingClass          :   'animsition-loading',
-      unSupportCss          : [ 'animation-duration',
-                                '-webkit-animation-duration',
-                                '-o-animation-duration'
-                              ],
-      //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-      //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+  //     inClass               :   'fade-in',
+  //     outClass              :   'fade-out',
+  //     inDuration            :    600,
+  //     outDuration           :    400,
+  //     linkElement           :   '.animsition-link',
+  //     // e.g. linkElement   :   'a:not([target="_blank"]):not([href^=#])'
+  //     loading               :    true,
+  //     loadingParentElement  :   'body', //animsition wrapper element
+  //     loadingClass          :   'animsition-loading',
+  //     unSupportCss          : [ 'animation-duration',
+  //                               '-webkit-animation-duration',
+  //                               '-o-animation-duration'
+  //                             ],
+  //     //"unSupportCss" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+  //     //The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
       
-      overlay               :   false,
+  //     overlay               :   false,
       
-      overlayClass          :   'animsition-overlay-slide',
-      overlayParentElement  :   'body'
-  });
+  //     overlayClass          :   'animsition-overlay-slide',
+  //     overlayParentElement  :   'body'
+  // });
   // Fit Vid
   $("#videoPlayer").fitVids();
 
